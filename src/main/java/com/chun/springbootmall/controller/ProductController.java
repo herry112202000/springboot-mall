865945e47b4840552e3cc97c4f45ee8d8a285b1e
herry112202000 <herry112202000@gmail.com>
2023-05-27 18:39:47 +0800

    「商品功能 - 實作查詢商品列表(分頁 + 總筆數)」

--- src/main/java/com/chun/springbootmall/controller/ProductController.java
+++ src/main/java/com/chun/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.chun.springbootmall.dto.ProductQueryParams;
 import com.chun.springbootmall.dto.ProductRequest;
 import com.chun.springbootmall.model.Product;
 import com.chun.springbootmall.service.ProductService;
+import com.chun.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -25,7 +26,7 @@ public class ProductController {
 
     @Validated
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -46,8 +47,19 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+
+
         List<Product> productList = productService.getProducts(productQueryParams);
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+
+        Integer total = productService.countProduct(productQueryParams);
+
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
